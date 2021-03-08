28fd488d51b56c36d75c0c3e1048e4afbbfc447e
savinn <savinn85@gmail.com>
2021-03-08 20:37:58 +0100

    reverted   http module import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HttpClient} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {ScrollingModule} from '@angular/cdk/scrolling';
 import { ChunkPipe } from './chunk.pipe';
 import { MatTooltipModule } from '@angular/material';
 
 @NgModule({
-  declarations: [
+  declarations: [	
     AppComponent,
       ChunkPipe
    ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     ScrollingModule,
-    MatTooltipModule,
-    HttpClient
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
